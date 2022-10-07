64088d9e1d05602c3f2d7f29b5f8987cb985b707
arlocomotive <113862557+arlocomotive@users.noreply.github.com>
2022-10-06 22:36:17 -0500

    cleaning up

---
+++ main.js
@@ -0,0 +1,47 @@
+const score = 0;
+
+const addAmountPerClick = 1;
+
+const upgradeAmount = 15;
+
+function playSound(sound) {
+
+    new Audio(sound).play();
+
+}
+
+function upgrade() {
+
+    if (score >= upgradeAmount) {
+
+        score = score - upgradeAmount
+
+        upgradeAmount = Math.round(upgradeAmount * 1.5)
+
+        addAmountPerClick = addAmountPerClick + 1
+
+        document.getElementById("score").innerHTML = score;
+
+        document.getElementById("upgradeAmount").innerHTML = upgradeAmount;
+
+        document.getElementById("addAmountPerClick").innerHTML = addAmountPerClick;
+
+        playSound("upgrade.wav")
+
+    } else {
+
+        playSound("no.wav")
+
+    }
+
+}
+
+function addToScore() {
+
+    score = score + addAmountPerClick;
+
+    document.getElementById("score").innerHTML = score;
+
+    playSound("pickup.wav")
+
+}
